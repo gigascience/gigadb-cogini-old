652852f3185612f5463de4862a4611d3f1a13f14
Stephane Nguyen <stephane@cogini.com>
2014-11-06 21:35:56 +0800

    Add all new models and also modified models for schemaV3

--- protected/models/Author.php
+++ protected/models/Author.php
@@ -14,8 +14,7 @@
  * The followings are the available model relations:
  * @property DatasetAuthor[] $datasetAuthors
  */
-class Author extends CActiveRecord
-{
+class Author extends CActiveRecord {
 	/**
 	 * Returns the static model of the specified AR class.
 	 * @param string $className active record class name.
@@ -23,42 +22,38 @@ class Author extends CActiveRecord
 	 */
 	public $dois_search;
 
-	public static function model($className=__CLASS__)
-	{
+	public static function model($className = __CLASS__) {
 		return parent::model($className);
 	}
 
 	/**
 	 * @return string the associated database table name
 	 */
-	public function tableName()
-	{
+	public function tableName() {
 		return 'author';
 	}
 
 	/**
 	 * @return array validation rules for model attributes.
 	 */
-	public function rules()
-	{
+	public function rules() {
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
 			array('surname', 'required'),
-			array('gigadb_user_id', 'numerical', 'integerOnly'=>true),
-			array('surname, middle_name, first_name', 'length', 'max'=>255),
-			array('orcid', 'length', 'max'=>128),
+			array('gigadb_user_id', 'numerical', 'integerOnly' => true),
+			array('surname, middle_name, first_name', 'length', 'max' => 255),
+			array('orcid', 'length', 'max' => 128),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
-			array('id, surname, middle_name, first_name, orcid, gigadb_user_id', 'safe', 'on'=>'search'),
+			array('id, surname, middle_name, first_name, orcid, gigadb_user_id', 'safe', 'on' => 'search'),
 		);
 	}
 
 	/**
 	 * @return array relational rules.
 	 */
-	public function relations()
-	{
+	public function relations() {
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
@@ -70,57 +65,54 @@ class Author extends CActiveRecord
 	/**
 	 * @return array customized attribute labels (name=>label)
 	 */
-	public function attributeLabels()
-	{
+	public function attributeLabels() {
 		return array(
-            'id' => 'ID',
-            'surname' => 'Surname',
-            'middle_name' => 'Middle Name',
-            'first_name' => 'First Name',
-            'orcid' => 'Orcid',
-            'gigadb_user_id' => 'Gigadb User',
-        );
+			'id' => 'ID',
+			'surname' => 'Surname',
+			'middle_name' => 'Middle Name',
+			'first_name' => 'First Name',
+			'orcid' => 'Orcid',
+			'gigadb_user_id' => 'Gigadb User',
+		);
 	}
 
 	/**
 	 * Retrieves a list of models based on the current search/filter conditions.
 	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
 	 */
-	public function search()
-	{
+	public function search() {
 		// Warning: Please modify the following code to remove attributes that
 		// should not be searched.
 
-		$criteria=new CDbCriteria;
+		$criteria = new CDbCriteria;
 
-		$criteria->compare('id',$this->id);
-		$criteria->compare('LOWER(surname)',strtolower($this->surname),true);
-		$criteria->compare('LOWER(middle_name)',strtolower($this->middle_name),true);
-		$criteria->compare('LOWER(first_name)',strtolower($this->first_name),true);
-		$criteria->compare('LOWER(orcid)',strtolower($this->orcid),true);
-		$criteria->compare('gigadb_user_id',$this->gigadb_user_id);
+		$criteria->compare('id', $this->id);
+		$criteria->compare('LOWER(surname)', strtolower($this->surname), true);
+		$criteria->compare('LOWER(middle_name)', strtolower($this->middle_name), true);
+		$criteria->compare('LOWER(first_name)', strtolower($this->first_name), true);
+		$criteria->compare('LOWER(orcid)', strtolower($this->orcid), true);
+		$criteria->compare('gigadb_user_id', $this->gigadb_user_id);
 
 		if ($this->dois_search) {
-			$matchedSql = 'SELECT dataset_id, author_id FROM dataset, dataset_author WHERE dataset.identifier LIKE \'%'.$this->dois_search.'%\' AND dataset.id = dataset_author.dataset_id';
-			$criteria->addInCondition('t.id',CHtml::listData(DatasetAuthor::model()->findAllBySql($matchedSql),'dataset_id','author_id'));
+			$matchedSql = 'SELECT dataset_id, author_id FROM dataset, dataset_author WHERE dataset.identifier LIKE \'%' . $this->dois_search . '%\' AND dataset.id = dataset_author.dataset_id';
+			$criteria->addInCondition('t.id', CHtml::listData(DatasetAuthor::model()->findAllBySql($matchedSql), 'dataset_id', 'author_id'));
 		}
 
 		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
+			'criteria' => $criteria,
 		));
 	}
 
-    public function getFullAuthor(){
-        //return $this->name . ' - ORCID:' . $this->orcid . ' - RANK:' . $this->rank;
-        return $this->first_name . ' ' . $this->surname . ' - ORCID:' . $this->orcid;
-    }
+	public function getFullAuthor() {
+		//return $this->name . ' - ORCID:' . $this->orcid . ' - RANK:' . $this->rank;
+		return $this->first_name . ' ' . $this->surname . ' - ORCID:' . $this->orcid;
+	}
 
-    /**
-     * Return first name and surname
-     * @return string
-     */
-    public function getName()
-    {
-    	return $this->surname . ', ' . $this->first_name;
-    }
+	/**
+	 * Return first name and surname
+	 * @return string
+	 */
+	public function getName() {
+		return $this->surname . ', ' . $this->first_name;
+	}
 }
